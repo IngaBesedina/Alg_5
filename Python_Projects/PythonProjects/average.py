45209df991f2c4c92d8619772afd1d2d54f7e7eb
IngaBesedina <144338827+IngaBesedina@users.noreply.github.com>
2023-10-09 12:36:15 +0300

    edit files

--- Python_Projects/PythonProjects/average.py
+++ Python_Projects/PythonProjects/average.py
@@ -14,9 +14,9 @@ N = 400
 k = 30
 x = np.array(range(10, N + 1, 10))
 
-t = []
 E = []
 q = []
+t = []
 
 for n in range(10, N + 1, 10):
     a = np.array(np.random.randint(-100, 100, n))
@@ -27,13 +27,16 @@ for n in range(10, N + 1, 10):
         t.append(end_time)
     E.append(1/k*np.sum(t))
     q.append(np.std(t))
-
+    t.clear()
 
 c = np.polyfit(x, E, 3)
 f = np.poly1d(c)
 x_new = np.linspace(x.min(), x.max(), 200)
 y_new = f(x_new)
 
-plt.plot(x_new, y_new, label='Fitted data', color='r')
+plt.plot(x_new, y_new, color='r')
 plt.errorbar(x, E, yerr=q, fmt='o', markersize=2, capsize=2)
+plt.xlabel('Размер массива')
+plt.ylabel('Время работы функции')
+plt.title('Средний случай')
 plt.show()
